f310aa9a5c1dd4677738a2490205526c64df4164
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-11 22:26:07 +0530

    Create hero folder

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>I am Meemaw</h1>
+            <p className={styles.description}>I'm a full-stack engineer  1year of Experience in React and Redux. Reach out if you'd like to learn more</p>
+            <a href="mailto:lawmsangivarte22@gmail.com" className={styles.contactBtn}></a>
+        </div>
+        <img src={getImageUrl("hero/heroImage.png")} alt="hero image" className={styles.heroImg} />
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+    </section>
+  )
+}
+
+export default Hero
