f569fbf21fa760ccf30386ec06c457106e1d99f7
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-12 23:52:10 +0530

    Add about,experience and projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import styles from './App.module.css'
+import About from './components/About/About'
+import Experience from './components/Experience/Experience'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
+import Projects from './components/Projects/Projects'
 
 function App() {
 
@@ -8,6 +11,9 @@ function App() {
     <div className={styles.App}>
       <Navbar />
       <Hero />
+      <About />
+      <Experience />
+      <Projects />
     </div>
   )
 }
