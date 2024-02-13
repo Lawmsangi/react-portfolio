b6d5ef8c3413284330d9b79ebc248c82d3aec0e8
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-13 22:57:37 +0530

    Change the name

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className={styles.container} id='home'>
         <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I am Meemaw</h1>
+            <h1 className={styles.title}>Hi, I am Lawmsangi</h1>
             <p className={styles.description}>I'm a full-stack engineer  1year of Experience in React and Redux. Reach out if you'd like to learn more</p>
             <a href="mailto:lawmsangivarte22@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
