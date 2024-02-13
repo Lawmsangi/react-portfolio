8b98e635f5c7f795bc5b513ee812a08a0a591c94
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-13 22:58:03 +0530

    Fix issues on mapping skills list

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -9,13 +9,17 @@ const ProjectCard = ({ project: {title, imageSrc, description, skills, demo, sou
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.description}>{description}</p>
         <ul className={styles.skills}>
-            {skills.map((skill,id)=>{
-                <li key={id}>{skill}</li>
-            })}
-        </ul>
+          {skills.map((skill, id) => {
+            return (
+              <li key={id} className={styles.skill}>
+                {skill}
+              </li>
+            );
+          })}
+      </ul>
         <div className={styles.links}>
-            <a href={demo} className={styles.link}>Demo</a>
-            <a href={source} className={styles.link}>Source</a>
+            <a href={demo} className={styles.link} target="_blank" rel="noopener noreferrer">Demo</a>
+            <a href={source} className={styles.link} target="_blank" rel="noopener noreferrer">Source</a>
         </div>
     </div>
   )
